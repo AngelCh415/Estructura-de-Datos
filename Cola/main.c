294b357df3e1dba113b62ca7bc76fffe059d7464
AngelCh415 <angelchavezmorones@gmail.com>
2020-03-11 23:12:53 -0600

    Cola con arreglos

---
+++ Cola/main.c
@@ -0,0 +1,18 @@
+#include "Cola.h"
+
+int main(void){
+	int tope=0;
+	int dato =0;
+	int n=0;
+	int ini=1;
+	printf("Ingrese el valor de la cola: ");
+	scanf("%d",&n); 
+	for(int i=0; i<n;i++){
+		printf("Ingrese el valor %d: ", i+1);
+		scanf("%d",&dato);
+		push(&cola, dato, &tope);
+	}
+	pop(&cola, &ini,tope);
+	pop(&cola, &ini,tope);
+	return 0;
+}

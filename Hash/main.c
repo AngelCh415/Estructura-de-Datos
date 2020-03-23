330872533788ac4d0597e7ac327b941dfb923ecb
AngelCh415 <angelchavezmorones@gmail.com>
2020-03-23 13:25:28 -0600

    Prueba Hash v2

--- Hash/main.c
+++ Hash/main.c
@@ -2,22 +2,19 @@
 
 int main(){
 	int dato = 0; 
-	int indice = 0;
 	int n = 0;
 	int i;
-//	struct Lista *lista; 
 	struct Tabla *tabla;
-	tabla = crear();
+	
+	tabla = crear(tabla);
 	printf("Ingrese cuantos valores se leeran ");
 	scanf("%d", &n);
 	for ( i=0; i<n; i++){
 		scanf ("%d", &dato); 
-		indice = hasheo(dato); 
-		tabla = insertar (tabla, indice, dato);
+		tabla = insertar (tabla, dato);
 	}
 	printf ("Ingrese el elemento a buscar: ");
 	scanf("%d", &dato);
-	indice = hasheo(dato); 
-	buscar(tabla,indice);
+	buscar(tabla,dato);
 	return 0;
 }

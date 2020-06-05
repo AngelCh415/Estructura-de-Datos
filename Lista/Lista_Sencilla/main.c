94cb712ed67b405167d4a97f5d582712ba720c21
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-05 17:15:37 -0500

    Lista senciilla

---
+++ Lista/Lista_Sencilla/main.c
@@ -0,0 +1,20 @@
+#include "Lista.h"
+
+int main(){
+	int i=0; 
+	int total=0;
+	int dato=0;
+	struct Lista *lista;
+	lista=NULL;
+	printf("Ingrese la cantidad de datos: ");
+	scanf("%d", &total);
+	scanf("%d", &dato);
+	lista= crear(dato);
+	for(i=1; i<total;i++){
+		scanf("%d",&dato);
+		lista= insertar(lista,dato);
+	}
+	printf("Los datos mostrados son: ");
+	mostrar(lista);
+	return 0;
+}

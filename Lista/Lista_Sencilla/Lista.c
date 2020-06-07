a1f35e7ba4073cb68942b5ca9822ad4a4e56c780
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-07 12:51:43 -0500

    Lista sencilla ya identada

--- Lista/Lista_Sencilla/Lista.c
+++ Lista/Lista_Sencilla/Lista.c
@@ -1,27 +1,34 @@
 #include "Lista.h"
 
-struct Lista * crear(int dato){
-	struct Lista *nueva= NULL;
-	nueva = (struct Lista *) malloc (sizeof(struct Lista));
-	if(nueva==NULL){
-		return NULL;
-	}
-	nueva->dato = dato;
-	nueva->siguiente = NULL;
-	return nueva;
+struct Lista *
+crear (int dato)
+{
+  struct Lista *nueva = NULL;
+  nueva = (struct Lista *) malloc (sizeof (struct Lista));
+  if (nueva == NULL)
+    {
+      return NULL;
+    }
+  nueva->dato = dato;
+  nueva->siguiente = NULL;
+  return nueva;
 }
 
-struct Lista *  insertar (struct Lista* lista, int dato){
-	struct Lista *nueva_lista = crear(dato);
-	nueva_lista->siguiente = lista;
-	lista= nueva_lista;
-	return lista;
+struct Lista *
+insertar (struct Lista *lista, int dato)
+{
+  struct Lista *nueva_lista = crear (dato);
+  nueva_lista->siguiente = lista;
+  lista = nueva_lista;
+  return lista;
 }
 
-void mostrar ( struct Lista* lista){
-	while(lista!=NULL){
-		printf("%d ", (*lista).dato);
-		lista= lista->siguiente;
-	}
+void
+mostrar (struct Lista *lista)
+{
+  while (lista != NULL)
+    {
+      printf ("%d ", (*lista).dato);
+      lista = lista->siguiente;
+    }
 }
-

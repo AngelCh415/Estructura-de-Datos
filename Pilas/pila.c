21de9c3c22b952033779e16596af380a4bb458c3
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-10 18:16:37 -0500

    Pila indentada

--- Pilas/pila.c
+++ Pilas/pila.c
@@ -1,12 +1,19 @@
 #include "pila.h"
 
-void insertar (int *pila,int dato,int *tope){
-	pila[++(* tope)]=dato;
+void
+insertar (int *pila, int dato, int *tope)
+{
+  pila[++(*tope)] = dato;
 }
-void mostrar_eliminar (int *pila, int *tope){
-	if((* tope)==-1) printf("Ya es toda la pila");
-	else{	
-		int x =pila[(* tope)--];
-		printf ("%d\n", x);
-	}
+
+void
+mostrar_eliminar (int *pila, int *tope)
+{
+  if ((*tope) == -1)
+    printf ("Ya es toda la pila");
+  else
+    {
+      int x = pila[(*tope)--];
+      printf ("%d\n", x);
+    }
 }

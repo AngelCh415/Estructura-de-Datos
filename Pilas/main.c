c7605638c8d8acca3512a611c340727764763990
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-10 18:15:28 -0500

    Pila ya indentada

--- Pilas/main.c
+++ Pilas/main.c
@@ -1,14 +1,18 @@
 #include "pila.h"
 
-int main (void){
-	
-	int tope=-1;
-	insertar (&pila, 5,&tope);
-	insertar (&pila, 15, &tope);
-	insertar (&pila, 20, &tope);
-	mostrar_eliminar(&pila, &tope);
-	mostrar_eliminar(&pila, &tope);
-	mostrar_eliminar(&pila, &tope);
-	mostrar_eliminar(&pila, &tope);
-	return 0;
+int
+main (void)
+{
+
+  int tope = -1;
+  printf ("Agregando elementos 5,15 y 20: \n");
+  insertar (pila, 5, &tope);
+  insertar (pila, 15, &tope);
+  insertar (pila, 20, &tope);
+  printf ("Mostrando y eliminando de la pila: \n");
+  mostrar_eliminar (pila, &tope);
+  mostrar_eliminar (pila, &tope);
+  mostrar_eliminar (pila, &tope);
+  mostrar_eliminar (pila, &tope);
+  return 0;
 }

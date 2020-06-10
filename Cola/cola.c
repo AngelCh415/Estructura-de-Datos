8ec7d6e68a70ad2891984d73765e9f25bae49e6d
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-10 17:59:49 -0500

    Cola con arreglo

--- Cola/cola.c
+++ Cola/cola.c
@@ -1,14 +1,21 @@
 #include "Cola.h"
 
-void push( int *cola, int dato, int *tope){
-	cola[++(* tope)] = dato;
+void
+push (int *cola, int dato, int *tope)
+{
+  cola[++(*tope)] = dato;
 }
-void pop (int *cola, int *ini, int tope){
-	if((* ini)==tope+1){
-		printf("Ya es toda la cola");
-	}
-	else{
-		int x = cola[(*ini)++];
-		printf("%d\n", x);
-	}
+
+void
+pop (int *cola, int *ini, int tope)
+{
+  if ((*ini) == tope + 1)
+    {
+      printf ("Ya es toda la cola");
+    }
+  else
+    {
+      int x = cola[(*ini)++];
+      printf ("%d\n", x);
+    }
 }

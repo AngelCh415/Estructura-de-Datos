72e8743f771c0bfcf7133208073027dc177d3aca
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-15 18:05:50 -0500

    Arbol con impresion

--- Arboles/BST/main.c
+++ Arboles/BST/main.c
@@ -9,5 +9,13 @@ main (void)
   arbol = insertar_Arbol (arbol, 100);
   arbol = insertar_Arbol (arbol, 15);
   arbol = insertar_Arbol (arbol, 150);
+  printf ("Impresion normal: \n");
+  mostrar_1 (arbol);
+  printf ("\n");
+  printf ("Impresion ordenada (menor a mayor): \n");
+  mostrar_2 (arbol);
+  printf ("\n");
+  printf ("Impresion ordenada (mayor a menor): \n");
+  mostrar_3 (arbol);
   return 0;
 }

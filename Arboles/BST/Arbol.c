d2a1da03111b42eb7bda9f77e2bb4ec9ff4086ff
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-15 18:02:39 -0500

    Arbol con impresion

--- Arboles/BST/Arbol.c
+++ Arboles/BST/Arbol.c
@@ -32,3 +32,28 @@ insertar_Arbol (struct Arbol *raiz, int dato)
     }
   return raiz;
 }
+void mostrar_1 (struct Arbol *raiz){
+  if(raiz==NULL){
+    return ;
+  }
+  printf ("%d ", raiz->dato);
+  mostrar_1(raiz->izquierda);
+  mostrar_1(raiz->derecha);
+}
+void mostrar_2 (struct Arbol *raiz){
+  if(raiz==NULL){
+    return ;
+  }
+  mostrar_2(raiz->izquierda);
+  printf ("%d ", raiz->dato);
+  mostrar_2(raiz->derecha);
+
+}
+void mostrar_3 (struct Arbol *raiz){
+  if(raiz==NULL){
+    return ;
+  }
+  mostrar_3(raiz->derecha);
+  mostrar_3(raiz->izquierda);
+  printf ("%d ", raiz->dato);
+}

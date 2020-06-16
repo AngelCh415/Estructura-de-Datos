329f5a0b3790240b721baf444637e3fe3a20d474
AngelCh415 <angelchavezmorones@gmail.com>
2020-06-16 16:23:38 -0500

    Lista funcionando

--- Lista/Lista_Doble/ListaD.h
+++ Lista/Lista_Doble/ListaD.h
@@ -4,15 +4,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct ListaD {
-	int dato;
-	struct ListaD * siguiente;
-	struct ListaD * anterior;
+struct ListaD
+{
+  int dato;
+  struct ListaD *siguiente;
+  struct ListaD *anterior;
 };
-struct ListaD * crear_lista_doble (int);
-struct ListaD * insertar_en_doble (struct ListaD * , int);
+struct ListaD *crear_lista_doble (int);
+struct ListaD *insertar_en_doble (struct ListaD *, int);
 void mostrar (struct ListaD *);
-void mostrar_re (struct ListaD *);
-struct ListaD * eliminar_en_doble (struct ListaD *);
+void mostrar_re (struct ListaD *, struct ListaD *);
+struct ListaD *eliminar_en_doble (struct ListaD *);
 
 #endif
--- Lista/Lista_Doble/main.c
+++ Lista/Lista_Doble/main.c
@@ -1,19 +1,23 @@
 #include "ListaD.h"
 
-int main(void){
-	struct ListaD * lista= NULL;
-	lista = crear_lista_doble(13);
-	lista = insertar_en_doble (lista, 12);
-	lista = insertar_en_doble (lista, 9);
-	lista = insertar_en_doble (lista, 2);
-	lista = insertar_en_doble (lista, 5);
-	mostrar(lista);
-	printf("\n");
-	mostrar_re(lista);
-	lista = eliminar_en_doble (lista);
-	printf("\n");
-	mostrar(lista);
-	printf("\n");
-	mostrar_re(lista);
-	return 0;
+int
+main (void)
+{
+  struct ListaD *lista = NULL;
+  struct ListaD *aux = NULL;
+  lista = crear_lista_doble (13);
+  aux = lista;
+  lista = insertar_en_doble (lista, 12);
+  lista = insertar_en_doble (lista, 9);
+  lista = insertar_en_doble (lista, 2);
+  lista = insertar_en_doble (lista, 5);
+  mostrar (lista);
+  printf ("\n");
+  mostrar_re (lista, aux);
+  lista = eliminar_en_doble (lista);
+  printf ("\n");
+  mostrar (lista);
+  printf ("\n");
+  mostrar_re (lista, aux);
+  return 0;
 }
